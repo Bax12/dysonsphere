31752f5651c0622d08bddcb5921e2b01ae6ec401
Bax12 <sebastian-dauner@web.de>
2026-04-02 23:30:36 +0200

    Changed Laser Cooldown TreeMap to PriorityQueue

--- src/main/java/de/bax/dysonsphere/network/LaserCooldownSyncPackage.java
+++ src/main/java/de/bax/dysonsphere/network/LaserCooldownSyncPackage.java
@@ -1,10 +1,13 @@
 package de.bax.dysonsphere.network;
 
+import java.util.ArrayList;
+import java.util.Collection;
 import java.util.Map;
 import java.util.TreeMap;
 import java.util.function.Supplier;
 
 import de.bax.dysonsphere.capabilities.DSCapabilities;
+import de.bax.dysonsphere.capabilities.orbitalLaser.OrbitalLaserPlayerContainer.OrbitalLaserContainer.LaserCooldown;
 import net.minecraft.client.Minecraft;
 import net.minecraft.nbt.CompoundTag;
 import net.minecraft.network.FriendlyByteBuf;
@@ -13,11 +16,11 @@ import net.minecraftforge.network.NetworkEvent;
 
 public class LaserCooldownSyncPackage {
     
-    public final Map<Integer, Integer> laserCooldowns;
+    public final Collection<LaserCooldown> laserCooldowns; //Serverside gametick is relative to servertick, clientside gametick is absolute 
     public final int serverGameTick;
     public final int dsLaserCount;
 
-    public LaserCooldownSyncPackage(Map<Integer, Integer> laserCooldowns, int serverGameTick, int dsLaserCount){
+    public LaserCooldownSyncPackage(Collection<LaserCooldown> laserCooldowns, int serverGameTick, int dsLaserCount){
         this.laserCooldowns = laserCooldowns;
         this.serverGameTick = serverGameTick;
         this.dsLaserCount = dsLaserCount;
@@ -25,21 +28,21 @@ public class LaserCooldownSyncPackage {
 
     public void encode(FriendlyByteBuf buf){
         buf.writeInt(laserCooldowns.size());
-        laserCooldowns.forEach((cooldown, laserCount) -> {
-            buf.writeInt(cooldown - serverGameTick);
-            buf.writeInt(laserCount);
+        laserCooldowns.forEach((cooldown) -> {
+            buf.writeInt(cooldown.gameTick() - serverGameTick);
+            buf.writeInt(cooldown.count());
         });
         buf.writeInt(dsLaserCount);
     }
 
     public static LaserCooldownSyncPackage decode(FriendlyByteBuf buf){
         int count = buf.readInt();
-        Map<Integer, Integer> map = new TreeMap<>();
+        Collection<LaserCooldown> cooldowns = new ArrayList<>(count);
         for(int i = 0; i < count; i++){
-            map.put(buf.readInt(), buf.readInt());
+            cooldowns.add(new LaserCooldown(buf.readInt(), buf.readInt()));
         }
         int dsLaserCount = buf.readInt();
-        return new LaserCooldownSyncPackage(map, 0, dsLaserCount);
+        return new LaserCooldownSyncPackage(cooldowns, 0, dsLaserCount);
     }
 
     public void handle(Supplier<NetworkEvent.Context> ctx){
@@ -49,10 +52,10 @@ public class LaserCooldownSyncPackage {
                     laser.load(new CompoundTag(), 0); //clearing the existing list
                     // DysonSphere.LOGGER.info("LaserCooldownSyncPacket handle laserCooldown Size: {}", laserCooldowns.size());
                     // DysonSphere.LOGGER.info("LaserCooldownSyncPacket handle dsLaserCount: {}", dsLaserCount);
-                    laserCooldowns.forEach((cooldown, laserCount) -> {
+                    laserCooldowns.forEach((cooldown) -> {
                         // DysonSphere.LOGGER.info("LaserCooldownSyncPacket handle single laserCooldown: {}", cooldown);
                         // DysonSphere.LOGGER.info("LaserCooldownSyncPacket handle single laserCount: {}", laserCount);
-                        laser.putLasersOnCooldown(Minecraft.getInstance().player.tickCount, laserCount, cooldown);
+                        laser.putLasersOnCooldown(Minecraft.getInstance().player.tickCount, cooldown.count(), cooldown.gameTick());
                     });
                     laser.setDysonSphereLaserCount(dsLaserCount);
                 });
